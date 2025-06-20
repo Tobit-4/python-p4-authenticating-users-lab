16a2dbc4faa8d0572385604aad510705a8463780
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-06-20 18:52:42 +0300

    made changes

--- server/app.py
+++ server/app.py
@@ -18,6 +18,38 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter_by(username = username).first()
+
+        if user:
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'message': 'User not found'}, 404
+api.add_resource(Login, '/login')
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+api.add_resource(Logout, '/logout')
+
+
+class CheckSession(Resource):   
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
 class ClearSession(Resource):
 
     def delete(self):
